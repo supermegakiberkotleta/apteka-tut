566ce22a48e84fa1ca02f711783e99dc5c7dfbac
vmedvedev <vmedvedev135@gmail.com>
2024-03-04 02:45:27 +0300

    add medicines table render

--- functions.php
+++ functions.php
@@ -8,4 +8,37 @@ function get_header(){
 function get_footer(){
    return require 'footer.php';
 }
+
+
+
+function getNameById($table, $id, $idFieldName) {
+    // Создание подключения
+    $conn = connectToDatabase();
+
+    // Защита от SQL-инъекций
+    $table = $conn->real_escape_string($table);
+    $id = $conn->real_escape_string($id);
+    $idFieldName = $conn->real_escape_string($idFieldName);
+
+    // Формирование запроса
+    $sql = "SELECT name FROM $table WHERE $idFieldName = $id";
+
+    // Выполнение запроса
+    $result = $conn->query($sql);
+
+    if ($result && $result->num_rows > 0) {
+        // Получение данных из результата запроса
+        $row = $result->fetch_assoc();
+        $name = $row['name'];
+    } else {
+        // Если элемент не найден, возвращаем null
+        $name = null;
+    }
+
+    // Закрытие подключения
+    $conn->close();
+
+    return $name;
+}
+
 ?>
--- medicines/index.php
+++ medicines/index.php
@@ -1,6 +1,33 @@
 <?php require_once  $_SERVER['DOCUMENT_ROOT'] . '/functions.php';?>
 <?get_header()?>
 <div class="container-fluid">
-    <h1 class="h3 mb-4 text-gray-800">Препараты</h1>
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">Препараты</h1>
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Название</th>
+                            <th>Цена</th>
+                            <th>Производитель</th>
+                            <th>Годен до</th>
+                            <th>Количество</th>
+                            <th>Категория</th>
+                            <!-- <th></th> -->
+                        </tr>
+                    </thead>
+                    <tbody data-table="medicines" id="tableRender">
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
 </div>
 <?get_footer()?>
